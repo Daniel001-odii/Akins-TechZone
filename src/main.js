d133e8b06714f427cc92e487c354ba4317d9bafd
Daniel001-odii <xenithheight@gmail.com>
2023-12-28 15:45:12 +0100

    fix general api_url

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ const app = createApp(App);
 
 
 
-app.config.globalProperties.api_url = 'https://tecnhzone-backend-c28bd32b1555.herokuapp.com/api';
+app.config.globalProperties.api_url = 'https://techzone-backend-c28bd32b1555.herokuapp.com/api';
 app.config.globalProperties.message_api_url = 'https://techzone-messaging-3b87ba218f74.herokuapp.com';
 
 
